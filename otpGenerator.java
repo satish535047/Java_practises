7fd2a701c6adb93aa7400b73bcae0ae9938a534c
sr535047 <you@example.com>
2023-03-28 17:45:23 +0530

    otp generator

---
+++ otpGenerator.java
@@ -0,0 +1,42 @@
+import java.util.Random;
+
+/*import java.util.Random;
+
+public class otpGenerator {
+
+    private static final int OTP_LENGTH = 6;
+
+    public static String generateOTP() {
+        Random r = new Random();
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<OTP_LENGTH; i++){
+            int digit = r.nextInt(0,10);
+            sb.append(digit);
+        }
+        return sb.toString();
+
+    }
+    public static void main(String[] args) {
+        String s = generateOTP();
+        System.out.println("your otp is :"+s);
+    }
+}*/
+
+public class otpGenerator{
+    private static final int OTP_LENGTH =4;
+
+    public static String generateOTP() {
+        Random r = new Random();
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<OTP_LENGTH;i++){
+            int digit = r.nextInt(0,10);
+            sb.append(digit);
+        }
+        return sb.toString();
+    }
+    public static void main(String[] args) {
+        String s = generateOTP();
+        System.out.println();
+        System.out.println("You'r Otp is :"+ s);
+    }
+}
