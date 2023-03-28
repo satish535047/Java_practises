20cc5afb6aa1758cc22cee014971b07e626edd2d
sr535047 <you@example.com>
2023-03-28 17:49:24 +0530

    comments have been removed

--- otpGenerator.java
+++ otpGenerator.java
@@ -1,27 +1,5 @@
 import java.util.Random;
 
-/*import java.util.Random;
-
-public class otpGenerator {
-
-    private static final int OTP_LENGTH = 6;
-
-    public static String generateOTP() {
-        Random r = new Random();
-        StringBuilder sb = new StringBuilder();
-        for(int i=0; i<OTP_LENGTH; i++){
-            int digit = r.nextInt(0,10);
-            sb.append(digit);
-        }
-        return sb.toString();
-
-    }
-    public static void main(String[] args) {
-        String s = generateOTP();
-        System.out.println("your otp is :"+s);
-    }
-}*/
-
 public class otpGenerator{
     private static final int OTP_LENGTH =4;
 
